51d976240fbe1557d115c386c062178ed2a1ec27
andrewdau21 <44346254+andrewdau21@users.noreply.github.com>
2022-05-15 11:24:21 -0400

    Switch to navbar page
    Switched to navbar page for better layout

--- ui.R
+++ ui.R
@@ -1,19 +1,10 @@
+ui <- navbarPage("2022 Garden",
+           tabPanel("Map",
+                    plotlyOutput("gardenmap")),
+           tabPanel("Table",
+                    dataTableOutput("fulltable"))
+           )
 
 
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-  
-  # Application title
-  titlePanel("2022 Garden"),
-  
-  # Sidebar with a slider input for number of bins 
- 
-    mainPanel(
-      plotlyOutput("gardenmap"),
-      #verbatimTextOutput("plantselect"),
-      dataTableOutput("fulltable")
-    )
-  
-)
 
